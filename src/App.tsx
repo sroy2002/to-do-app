dca98c7761dac027a809e29888e6bbb6d49a1790
Suranjana Roy <roysupra75@gmail.com>
2025-01-27 20:15:56 +0530

    completed the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { GoCheckCircleFill, GoCircle } from "react-icons/go";
+import { IoMdTrash } from "react-icons/io";
 
 type Task = {
   text: string;
@@ -15,6 +16,10 @@ function App() {
     setInputValue(e.target.value);
   };
   const handleAddBtn = () => {
+    if (inputValue === "") {
+      alert("Empty task not allowed!");
+      return;
+    }
     setTasks((prev) => [{ text: inputValue, completed: false }, ...prev]);
     setInputValue("");
   };
@@ -31,58 +36,63 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <h1 className="m-4 text-3xl">QuickTask</h1>
+    <div className="w-full h-screen flex flex-col  items-center gradient-background">
+      <div className="flex justify-center items-center my-8">
+        <h1 className="m-4 text-5xl quicksand-600 text-[#577BC1]">QuickTask</h1>
       </div>
-      <div className="m-4 flex gap-2">
+      <div className="my-3 mb-[3rem] flex gap-2 justify-center items-center w-[40%]">
         <input
           type="text"
           value={inputValue}
-          className="border-2 border-black"
+          className="border border-gray-300 rounded-full w-full px-8 py-3 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 outline-none quicksand-600"
           onChange={handleInputChange}
           placeholder="Enter your task"
         />
         <button
           type="button"
-          className="border-2 border-black px-4"
+          className="border-none bg-[#344CB7] px-8 py-3  text-white quicksand-600 font-bold rounded-full hover:scale-105 transition-all duration-300 ease-in-out hober:shadow-md"
           onClick={() => handleAddBtn()}
         >
           Add
         </button>
       </div>
-    { tasks.length>0 && <div className="flex items-center gap-4">
-        <p
-          onClick={() => {
-            setAllTasks(true);
-            setAllCompletedTasks(false);
-          }}
-          className={`cursor-pointer pb-1 ${
-            allTasks
-              ? "border-b-4 border-blue-500"
-              : "border-b-4 border-transparent"
-          }`}
-        >
-          All Tasks
-        </p>
-        <p
-          onClick={() => {
-            setAllTasks(false);
-            setAllCompletedTasks(true);
-          }}
-          className={`cursor-pointer pb-1 ${
-            allCompletedTasks
-              ? "border-b-4 border-blue-500"
-              : "border-b-4 border-transparent"
-          }`}
-        >
-          Completed Tasks
-        </p>
-      </div>}
+      {tasks.length > 0 && (
+        <div className="flex items-center justify-around gap-8 mb-8 w-[40%] quicksand-600">
+          <p
+            onClick={() => {
+              setAllTasks(true);
+              setAllCompletedTasks(false);
+            }}
+            className={`cursor-pointer pb-1 text-xl text-[#344CB7] ${
+              allTasks
+                ? "border-b-4 border-[#344CB7]"
+                : "border-b-4 border-transparent"
+            }`}
+          >
+            All Tasks
+          </p>
+          <p
+            onClick={() => {
+              setAllTasks(false);
+              setAllCompletedTasks(true);
+            }}
+            className={`cursor-pointer pb-1 text-xl text-[#344CB7] ${
+              allCompletedTasks
+                ? "border-b-4 border-[#344CB7]"
+                : "border-b-4 border-transparent"
+            }`}
+          >
+            Completed Tasks
+          </p>
+        </div>
+      )}
       {allTasks && (
-        <div>
+        <div className="w-[40%] text-lg montserrat-500 px-6">
           {tasks.map((task, index) => (
-            <div key={index} className=" flex gap-2 items-center">
+            <div
+              key={index}
+              className=" flex gap-2 items-center  border-b-2 border-gray-100 pb-2 mb-2"
+            >
               <div
                 onClick={() => {
                   toggleTask(index);
@@ -91,44 +101,51 @@ function App() {
               >
                 {task.completed ? <GoCheckCircleFill /> : <GoCircle />}
               </div>
-              <div
-                className={`${
-                  task.completed ? "line-through" : ""
-                } cursor-pointer`}
-              >
-                {task.text}
-              </div>
-              <div
-                className="text-red-500 hover:cursor-pointer"
-                onClick={() => handleDelete(index)}
-              >
-                {" "}
-                delete
+              <div className="flex justify-between items-center w-full">
+                <div
+                  className={`${
+                    task.completed ? "line-through" : ""
+                  } cursor-pointer`}
+                >
+                  {task.text}
+                </div>
+                <div
+                  className="text-red-500 hover:cursor-pointer text-lg"
+                  onClick={() => handleDelete(index)}
+                >
+                  {" "}
+                  <IoMdTrash />
+                </div>
               </div>
             </div>
           ))}
         </div>
       )}
       {allCompletedTasks && (
-        <div>
+        <div className="w-[40%] text-lg montserrat-500 flex flex-col px-6">
           {tasks
             .filter((task) => task.completed)
             .map((task, index) => (
-              <div key={index} className="flex gap-2 items-center">
+              <div
+                key={index}
+                className="flex gap-2 items-center border-b-2 border-gray-100 pb-2 mb-2"
+              >
                 <GoCheckCircleFill />
-                <div className="line-through cursor-pointer">{task.text}</div>
-                <div
-                  className="text-red-500 hover:cursor-pointer"
-                  onClick={() => handleDelete(index)}
-                >
-                  {" "}
-                  delete
+                <div className="flex justify-between items-center w-full">
+                  <div className="line-through cursor-pointer">{task.text}</div>
+                  <div
+                    className="text-red-500 hover:cursor-pointer text-lg "
+                    onClick={() => handleDelete(index)}
+                  >
+                    {" "}
+                    <IoMdTrash />
+                  </div>
                 </div>
               </div>
             ))}
         </div>
       )}
-    </>
+    </div>
   );
 }
 
